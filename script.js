5592e967f90f323c7afe9044bace3d461781ce1b
Iflaq khurshid <112414810+Iflaqbhat@users.noreply.github.com>
2024-10-19 15:42:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+const incrementBtn = document.getElementById('incrementBtn');
+
+// Add an event listener for the 'click' event
+incrementBtn.addEventListener('click', function() {
+let counterElement = document.getElementById("counter");
+let currentValue = parseInt(counterElement.innerText) || 0;  // Start with 0 if empty
+counterElement.innerText = currentValue + 1;
+	counterElement.innerText = currentValue;
+    alert(currentValue);
+});
